4a1f0876a2b04e8dec27ecf8956df87a08ccd8af
nayara <na.yaracarvalho1997@hotmail.com>
2025-06-21 21:34:57 -0300

    infracao finalizada

--- src/controllers/InfracaoController.js
+++ src/controllers/InfracaoController.js
@@ -13,6 +13,17 @@ class InfracaoController {
         const result = await InfracaoService.adicionar(req.body);
         return handleResult(result, res, 201); // 201: Created
     });
+     obterPorId = asyncWrapper(async (req, res) => {
+            const { id } = req.params;
+            const result = await InfracaoService.obterPorId(id);
+            return handleResult(result, res); // 200: Success
+        })
+    alterar = asyncWrapper(async (req, res) => {
+        const { id } = req.params;
+        const result = await InfracaoService.alterar(id, req.body);
+        return handleResult(result, res); // 200: Success
+    });
+
  
 }
 
